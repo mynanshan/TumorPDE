c653b5069ce159884eaa9f4ba217d4b086ef1e91
mynanshan <muye.nanshan@gmail.com>
2025-02-15 18:49:50 -0800

    update registration code

--- examples/patient-reconstruct/run_registration.sh
+++ examples/patient-reconstruct/run_registration.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-# Define the input file
-scan_count_file="scan_counts.txt"
+source settings.sh
+
+# Define the setting file
 ref_id_file="registration_ref_id.txt"
+# Define the metainfo file
+metainfo="${DATA_DIR}/patient_list.txt"
+
+# Determine which batch to be processed
+if [ $# -eq 0 ]; then    # No argument provided
+    target_batch=-1
+elif [ $# -eq 1 ]; then  # One argument provided, set x to the argument
+    target_batch=$1
+else
+    echo "Only 1 argument can be accepted. Usage: $0 <Target Batch ID>."
+    exit 1
+fi
 
 # Read ref_id.txt into an associative array
 declare -A ref_ids
@@ -11,15 +24,25 @@ while IFS=',' read -r patient ref_id; do
 done < "$ref_id_file"
 
 # Loop through each line in the input file
-while IFS=',' read -r path patient count; do
+while IFS=',' read -r name date1 date2 date3 datatype batchid numscan; do
+    # skip the header
+    if [ "$name" == "name" ]; then
+        continue
+    fi
+    
+    # if target_id is provided, skip non-target patients
+    if [ "$target_batch" -ne -1 ] && [ "$batchid" -ne "$target_batch" ]; then
+        continue  # Skip to the next iteration
+    fi
+
     # Check if the patient exists in the ref_id associative array
-    if [[ -n "${ref_ids[$patient]}" ]]; then
-        ref_id="${ref_ids[$patient]}"
+    if [[ -n "${ref_ids[$name]}" ]]; then
+        ref_id="${ref_ids[$name]}"
     else
         ref_id=1
     fi
 
     # Submit the job using sbatch with patient and scan_id as arguments
-    echo "Run skull-stipping for patient:" "$patient" "num scans:" "$count" "ref id:" "$ref_id"
-    sbatch registration.py "$patient" "$count" "$ref_id"
-done < "$scan_count_file"
+    echo "Run registration for patient:" "$name" "num scans:" "$numscan" "ref id:" "$ref_id"
+    sbatch registration.py "$name" "$numscan" "$ref_id"
+done < "$metainfo"
